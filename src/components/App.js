4f08443cd17bd193f84db61ec121bf13e3bf42a3
sahil n <sahil@mutterfly.in>
2018-12-04 16:56:22 +0530

    sets up some version of scroll paging. yet to be fixed

--- src/components/App.js
+++ src/components/App.js
@@ -5,16 +5,38 @@ import ImageList from './ImageList';
 
 class App extends React.Component {
 
-    state = { images: [] };
+    state = { images: [], page: 0, term: '' };
+
+    constructor(props) {
+        super(props);
+
+        window.onscroll = () => {
+            const {
+                loadUsers,
+                
+              } = this;
+        
+              // Bails early if:
+              // * there's an error
+              // * it's already loading
+              // * there's nothing left to load
+        
+              // Checks that the page has scrolled to the bottom
+              if ( window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight ) {
+                  console.log('load next');
+                  this.onSearchSubmit(this.state.term, (this.state.page + 1))
+              }
+            }
+    }
 
     // async await syntax used to make the asynchronous request.
     // onSearchSubmit is refactored into an arrow function to fix the context of 'this'.
-     onSearchSubmit = async (term) => {
+    onSearchSubmit = async (term, page) => {
         const response = await unsplash.get('/search/photos', {
-            params: { query: term },
+            params: { query: term, page },
         });
 
-        this.setState({images: response.data.results});
+        this.setState({images: this.state.images.concat(response.data.results), page, term});
     }
 
     render() {
