05b3cdfcd8de53455f22e71db9739f8d4364bf0b
sahil n <sahil@mutterfly.in>
2018-12-04 12:45:17 +0530

    fixes css styling to dynamically adapt

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,16 +1,17 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
     const images = props.images.map((image)=>{
-        const { description, id, urls } = image;
-        return <img 
-                    src={urls.regular} 
-                    alt={description}
-                    key={id}/>
+        return <ImageCard 
+                    key={image.id}
+                    image={image}
+                    />
     })
 
     return (
-        <div>{ images }</div>
+        <div className="image-list">{ images }</div>
     )
 }
 
