223a6ba8dcd17650c6dafc2969b29df79e08326e
sahil n <sahil@mutterfly.in>
2018-12-08 17:47:49 +0530

    displays user name and likes

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -40,9 +40,32 @@ class ImageCard extends React.Component {
                 <div className="iron-image-loaded visible content" 
                     ref={imageLoadedElem => this.newImghd = imageLoadedElem}>
                 </div>
-                <div className="iron-image-preload hidden content" 
-                     style={{ backgroundImage: `url('${this.props.image.urls.thumb}')`, filter:`blur(5px)` }}>
+                <div className="hidden content">
+                    <div className="iron-image-preload" 
+                        style={{ backgroundImage: `url('${this.props.image.urls.thumb}')`, filter: 'blur(8px)',
+                                height: '100%', backgroundPosition: 'center', backgroundRepeat: 'no-reapeat',
+                                backgroundSize: 'cover'}}>
+                    </div>
+                    <div className="bg-text">
+                        <div className="item">
+                            <div className="ui tiny circular image" style={{float: 'left'}}>
+                                <img src={this.props.image.user.profile_image.large} alt="user" />
+                            </div>
+                            <div className="content">
+                                <div className="header">
+                                    {this.props.image.user.first_name + ' '+ this.props.image.user.last_name}
+                                </div>
+                                <div className="meta">
+                                <span> 
+                                    <i className="heart icon"></i> {this.props.image.user.total_likes}
+                                </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
+                
             </div>
         )
     }
